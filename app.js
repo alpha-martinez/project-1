b54584f744cbd50327086644179bc2cb989b464a
alpha-martinez <martinezalphaa@gmail.com>
2020-07-26 17:36:46 -0400

    made a notes file and made variables on js

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-//Snake game: What you need to do? -->
+//name your variables for snake, game, canvas
+let snake; //my snake 
+let game; //a game variable
+let ctx; //a canvas function
+let scoreBoard; //a function to keep score
 
-//Step 1: Get your HTML, CSS, and JS all connected [DONE]
 
-//Step 2: start mapping HTML and CSS [starting...]
+//add a document listener here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM Loaded!");
+    
+})
 
-//Step: Figuring out how to set your HTML...do i use canvas?
 
-//MAJOR KEY 1: figure out how to move the snake around
-
-//MAJOR KEY 2: figure out how to randomly place a block around the grid 
-
-//MAJOR KEY 3: storage system? I need to take into consideration that when a snake eats a "apple"/"boxes", it gets added to its tail
-
-//MAJOR KEY 4: score board to keep track of how many player eats. 
-
-//MAJOR 5: find a function where when the snake hits a wall, it resets the game
-
-//step 6: find a function where when snake hits himself, game resets
