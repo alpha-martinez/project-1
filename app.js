2bf72bc9385e5bb5a2d95036a7af27cef29c6799
alpha-martinez <martinezalphaa@gmail.com>
2020-07-27 17:33:17 -0400

    fixing my arrow keys and attempting to add speed

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+//global variables
 let movementDisplay;
 let ctx
 let game;
-let hero;
-let ogre;
+let snake;
+let apple;
+let snakeBody = []; //have an array for the snake body
 
-// Crawler Constructor function
+
+//name arrow keys as variables to make readable
+// const up_arrow = 38;
+// const down_arrow = 40;
+// const right_arrow = 39;
+// const left_arrow = 37;
+
+// this creates new characters
 function Crawler(x, y, width, height, color) {
   this.x = x;
   this.y = y;
@@ -16,7 +25,24 @@ function Crawler(x, y, width, height, color) {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
+  this.direction = function() {
+        this.x += this.speedX
+        this.y += speedY
+  }
 }
+
+
+//call a function so you can create the snakeBody as you go 
+ let createBody = (snakeBody) => {
+    ctx.fillStyle = 'lightred';
+    ctx.strokestyle = 'darkred';
+
+    ctx.fillRect(snakeBody.x, snakeBody.y, 20, 20);
+    ctx.strokeRect(snakeBody.x, snakeBody.y, 20, 20);
+}
+//append onto the body as it eats something so that the rest of the body shows up 
+
+
 
 const detectHit = () => {
     //check for collision on x-axis
@@ -28,6 +54,8 @@ const detectHit = () => {
     ) { apple.alive = false;
     }
 }
+
+
 const gameLoop = () => {
   // clear the cavas
   ctx.clearRect(0, 0, game.width, game.height);
@@ -41,26 +69,39 @@ const gameLoop = () => {
   // render the snake
   snake.render()
 }
+
+this.moveDirection = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+}
+
+
 const movementHandler = e => {
   console.log(e)
   // w: 87, a:65, s:83, d:68
   switch (e.keyCode) {
     case (38): // up arrow key
-      if (snake.y > 0) snake.y -=5
+      snake.speedX = 0;
+      snake.speedY += 1;
       break;
     case (40): // down arrow key
-      if (snake.y + snake.height < game.height) snake.y +=5
+      snake.speedX = 0;
+      snake.speedY += 1;
       break;
     case (37): // left arrow key
-      if (snake.x > 0) snake.x -=5
+        snake.speedX += 1;
+        snake.speedY = 0;
       break;
     case (39): // right arrow key
-      if (snake.x + snake.width < game.width) snake.x +=5
+        snake.speedX += 1;
+        snake.speedY = 0;
       break;
     default:
       console.log('invalid');
   }
 }
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Dom loaded')
@@ -73,14 +114,11 @@ document.addEventListener('DOMContentLoaded', () => {
   ctx = game.getContext('2d');
   // CHARACTER REFS
   apple = new Crawler(300, 100, 15, 15, 'red');
-  snake = new Crawler(200, 100, 20, 20, 'yellow');
+  snake = new Crawler(150, 150, 20, 20, 'yellow');
   document.addEventListener('keydown', movementHandler);
   let runGame = setInterval(gameLoop, 60);
 })
 
 
-//have an array within the snake, snake body array that when the snake eats something
-    
 
-//append onto the body as it eats something so that the rest of the body shows up 
 
